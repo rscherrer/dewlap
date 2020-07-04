182d5841217cdfcd50aa8ab30f816212f0322164
rscherrer <raphael.scherrer@evobio.eu>
2020-07-04 15:44:45 +0200

    changing location content for the ms

---
+++ ms/fetch_content.sh
@@ -0,0 +1,45 @@
+# Fetch figures
+cp ../analyses/.../classif_svm_pca.png figures
+cp ../analyses/.../figure_anova.png figures
+cp ../analyses/.../map.pdf figures
+
+# Fetch supplementary figures
+cp ../analyses/.../classif_lda_pca.png suppfigures
+cp ../analyses/.../classif_lda_pca_pooled.png suppfigures
+cp ../analyses/.../classif_lda_refl.png suppfigures
+cp ../analyses/.../classif_lda_refl_pooled.png suppfigures
+cp ../analyses/.../classif_svm_pca_pooled.png suppfigures
+cp ../analyses/.../classif_svm_refl.png suppfigures
+cp ../analyses/.../classif_svm_refl_pooled.png suppfigures
+cp ../analyses/.../detailed_map.pdf suppfigures
+cp ../analyses/.../figure_brightness.png suppfigures
+cp ../analyses/.../figure_brightness_pooled.png suppfigures
+cp ../analyses/.../figure_contrasts.png suppfigures
+cp ../analyses/.../figure_distances2.png suppfigures
+cp ../analyses/.../figure_distances.png suppfigures
+cp ../analyses/.../figure_pooled.png suppfigures
+cp ../analyses/.../figure_reflectance.png suppfigures
+cp ../analyses/.../importance_lda_pca.png suppfigures
+cp ../analyses/.../importance_lda_pca_pooled.png suppfigures
+cp ../analyses/.../importance_lda_refl.png suppfigures
+cp ../analyses/.../importance_lda_refl_pooled.png suppfigures
+cp ../analyses/.../importance_svm_pca.png suppfigures
+cp ../analyses/.../importance_svm_pca_pooled.png suppfigures
+cp ../analyses/.../importance_svm_refl.png suppfigures
+cp ../analyses/.../importance_svm_refl_pooled.png suppfigures
+
+# Fetch tables
+cp ../analyses/07-ANOVA/table_anova.tex tables
+
+# Fetch supplementary tables
+cp ../analyses/01-counts/table_counts.tex supptables
+cp ../analyses/08-spatial-correlation/table_sites.tex supptables
+cp ../analyses/03-PCA/table_expvar.tex supptables
+cp ../analyses/03-PCA/table_brightness.tex supptables
+cp ../analyses/05-assumptions/table_multinorm.tex supptables
+cp ../analyses/05-assumptions/table_covariance.tex supptables
+cp ../analyses/05-assumptions/table_normality.tex supptables
+cp ../analyses/06-pooled/table_anova_pooled.tex supptables
+cp ../analyses/04-machine-learning/table_classif_svm_pca.tex supptables
+cp ../analyses/07-ANOVA/table_kw.tex supptables
+cp ../analyses/08-spatial-correlation/table_spatial.tex supptables
