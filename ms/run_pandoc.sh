7215c6247594aa6164ecb8c99e6d608927a4f54f
rscherrer <raphael.scherrer@evobio.eu>
2020-07-12 23:13:28 +0200

    second review

--- ms/run_pandoc.sh
+++ ms/run_pandoc.sh
@@ -1,4 +1,5 @@
 # Run this script to convert LaTeX files into ODT and DOCX documents
 # Requires pandoc and pandoc-crossref being installed
 
-pandoc main.tex --filter=pandoc-crossref --bibliography=library.bib --csl=journal-of-evolutionary-biology.csl -o scherrer2020.odt -o scherrer2020.docx
+pandoc main.tex --filter=pandoc-crossref --bibliography=library.bib --csl=journal-of-evolutionary-biology.csl -o scherrer2020.odt
+pandoc main.tex --filter=pandoc-crossref --bibliography=library.bib --csl=journal-of-evolutionary-biology.csl -o scherrer2020.docx
